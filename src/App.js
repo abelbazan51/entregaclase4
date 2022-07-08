bb50868f9909d15e3aa48f35a4dffb580f5e4d0f
abel bazan <abelbazan51@gmail.com>
2022-07-07 22:06:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import NavBar from "./navbar";
+import "./App.css";
+import Curso from "./Curso";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <NavBar />
+      </div>
+
+      <Curso />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Curso.js
@@ -0,0 +1,6 @@
+import "./App.css";
+
+const Curso = (Curso) => {
+  return <h2>bienvenidos a piluchina</h2>;
+};
+export default Curso;
