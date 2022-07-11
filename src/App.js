2d1e818923122a2846d6aec62ed71fc8f03625fc
abel bazan <abelbazan51@gmail.com>
2022-07-11 08:50:19 -0300

    video

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import NavBar from "./navbar";
 import "./App.css";
-import Curso from "./Curso";
-
-function App() {
+import Video from "./components/video";
+const App = () => {
   return (
     <>
-      <div className="app">
-        <NavBar />
-      </div>
-
-      <Curso />
+      <header>
+        <strong>
+          <em>react-tube-app</em>
+        </strong>
+      </header>
+      <Video
+        thumbnail="https://i.ytimg.com/vi/zQ41hq1V0Kk/mqdefault.jpg"
+        title="red stewart - young turks"
+      />
+      <Video />
     </>
   );
-}
-
+};
 export default App;
