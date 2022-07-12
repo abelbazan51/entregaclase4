d3a65ae624e4cd6b0117d1b05646a31c665a50bd
abel bazan <abelbazan51@gmail.com>
2022-07-12 19:59:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import "./App.css";
-import Video from "./components/video";
+
+import Navbar from "./components/navbar";
+import { ItemlistContainer } from "./containers/ItemListContainer";
+
 const App = () => {
   return (
     <>
-      <header>
-        <strong>
-          <em>react-tube-app</em>
-        </strong>
-      </header>
-      <Video
-        thumbnail="https://i.ytimg.com/vi/zQ41hq1V0Kk/mqdefault.jpg"
-        title="red stewart - young turks"
-      />
-      <Video />
+      <Navbar />
+      <ItemlistContainer />
     </>
   );
 };
+
 export default App;
--- src/Curso.js
+++
@@ -1,6 +0,0 @@
-import "./App.css";
-
-const Curso = (Curso) => {
-  return <h2>bienvenidos a piluchina</h2>;
-};
-export default Curso;
